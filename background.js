9cd0b3941cea22abdf6a5571c64a9a5148029a05
Denis Georgiev <d1bg@tuta.io>
2025-10-19 19:17:47 +0300

    a lot of useless code

--- background.js
+++ background.js
@@ -4,27 +4,6 @@
  * and inject the content script into YouTube and YouTube Music pages when they load.
  */
 
-// This listener fires whenever a tab is updated.
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // We only want to inject the script when the page has finished loading.
-  if (changeInfo.status === 'complete') {
-    // Check if the URL is a YouTube video or YouTube Music.
-    const isYouTube = tab.url && tab.url.includes("youtube.com/watch");
-    const isYouTubeMusic = tab.url && tab.url.includes("music.youtube.com");
-
-    if (isYouTube || isYouTubeMusic) {
-      // Inject the content script into the active tab.
-      browser.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ['content.js']
-      }).then(() => {
-        console.log("Youtube MPRIS Fixer: Injected content script into YouTube page.");
-      }).catch(err => {
-        console.error("Youtube MPRIS Fixer: Failed to inject script:", err);
-      });
-    }
-  }
+browser.runtime.onInstalled.addListener(() => {
+    console.log("MPRIS Fixer extension has been successfully installed or updated.");
 });
-
-
-
